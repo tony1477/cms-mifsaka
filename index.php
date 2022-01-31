725e7a8655c26cf0637b6960e2b2d7d98220728b
Martoni F <martoni.firman@nttindonesia.com>
2022-01-31 14:46:02 +0700

    First Initial

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$capella = dirname(__FILE__).'/protected/components/CpHelper.php';
+$yii=dirname(__FILE__).'/../framework/yii.php';
+$config=dirname(__FILE__).'/protected/config/main.php';
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+require_once($capella);
+require_once($yii);
+class ExtendableWebApp extends CWebApplication {
+	protected function init() {
+		foreach (glob(dirname(__FILE__).'/protected/modules/*', GLOB_ONLYDIR) as $moduleDirectory) {
+						$this->setModules(array(basename($moduleDirectory)));
+		}
+		return parent::init();
+	}
+}
+
+$app=new ExtendableWebApp($config);
+$app->run();
